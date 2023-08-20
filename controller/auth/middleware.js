08d334ab002f572b1c3cbd48cc6dff748e001aef
github_username <email_address>
2023-08-20 12:12:23 +0700

    commit_message

---
+++ controller/auth/middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'secret_key'); // 'secret_key' bisa diganti
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
+
+module.exports = { verifyToken };
